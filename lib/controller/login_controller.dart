397890b6b58c0edb68c56430aedc683870225eb7
Mohamad-Ammis <esd67618@gmail.com>
2023-11-20 22:36:11 +0200

    first commit

--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -1,13 +1,12 @@
 import 'package:get/get.dart';
 
-class LoginController extends GetxController{
-  bool obscureText=true;
-  void showPassword(){
-    if(obscureText==true){
-      obscureText=false;
-    }
-    else{
-      obscureText=true;
+class LoginController extends GetxController {
+  bool obscureText = true;
+  void showPassword() {
+    if (obscureText == true) {
+      obscureText = false;
+    } else {
+      obscureText = true;
     }
     update();
   }
